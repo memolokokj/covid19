b00a012c7d666c70c483a6cec09ef7d2876b4258
ParadoX <62449690+memolokokj@users.noreply.github.com>
2021-06-02 19:33:15 -0600

    infor map
    asd

--- js/sketch.js
+++ js/sketch.js
@@ -1,75 +1,111 @@
-const key = 'pk.eyJ1IjoiYXZyZW1pZ3VlIiwiYSI6ImNrN2UyaGdyZzA0NmozZ250bGNyMHMyaXYifQ.mxEBxZOBHLkzJGPJllpVEg';
-
-let canvas;
-let polygons = [];
-const mappa = new Mappa('MapboxGL', key);
-let trainMap;
-
-
-
-const options = {
-  lat: 0,
-  lng: 0,
-  zoom: 4,
-  style: 'mapbox://styles/mapbox/traffic-night-v2',
-  //pitch: 50,
-};
+var map;
+var infectados = {};
 
 function preload() {
 	const url = "https://api.apify.com/v2/key-value-stores/vpfkeiYLXPIDIea2T/records/LATEST"
-	const url2 = "assets/JSONS/states.json"
 	data = loadJSON(url);
-	polygonsData = loadJSON(url2);
 }
 
 function setup() {
-	canvas = createCanvas(800, 600);
-  trainMap = mappa.tileMap(options);
-  trainMap.overlay(canvas);
- // console.log(polygonsData);
-  noLoop();
+	var canvas = createCanvas(windowWidth/2, windowHeight/2);
 
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYXZyZW1pZ3VlIiwiYSI6ImNrN2UyaGdyZzA0NmozZ250bGNyMHMyaXYifQ.mxEBxZOBHLkzJGPJllpVEg';
+	map = new mapboxgl.Map({
+		container: "map",
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [-100.486052, 37.830348],
+		zoom: 2
+	});
+
+	state();
+	noLoop();
 }
 
 function draw() {
-	clear();
-
-	Object.entries(polygonsData.features).map( (s) => {
-		let [features, dato2] = s;
-		var array = [];
-		for(var i = 0; i <dato2.geometry.coordinates[0][0].length; i++){
-			array.push(dato2.geometry.coordinates[0][0][i]);
-		}
-		polygons.push(array);
-	});
-/*
-	//print(polygons);
-
-	for(var i = 0; i <polygons.length; i++){
-		for(var j = 0; j <polygons[i].length; j++){
-			beginShape();
-      fill("red");
-			var pos = trainMap.latLngToPixel(polygons[i][j][0], polygons[i][j][1]);
-      ellipse(pos.x, pos.y, 100, 100);
-			endShape();
-		}
-	}*/
-	console.log(polygons);
-
-	let nigeria = trainMap.latLngToPixel(11.396396, 5.076543);
-	console.log(nigeria);
-
-
-	/*print(data);
-	let count = 20;
+	background("black");
 	Object.entries(data.State).map( (s) => {
 		let [estado, datos] = s;
-		if(datos.infected <= 10000) fill("green");
-		else if(datos.infected > 15000 && datos.infected <20000) fill("orange");
-		else fill("red");
-		text(`${estado}: ${datos.infected} infectados`, 100, count);
-		count += 20;
-		console.log(datos.infected); 
+		infectados[estado] = datos.infected;
+	});
+	console.log(infectados); 
+}
 
-	});*/
+function state(){
+	var hoveredStateId = null;
+	 
+	map.on('load', function () {
+		// Create a popup, but don't add it to the map yet.
+	  var popup = new mapboxgl.Popup({
+	    closeButton: false,
+	    closeOnClick: false
+	  });
+
+	  function checkEmpty(info) {
+	    return (info) ? info : "No data";
+	  }
+
+		map.addSource('states', {
+			'type': 'geojson',
+			'data': 'assets/jsons/states.json'
+		});
+		
+		map.addLayer({
+			'id': 'state-fills',
+			'type': 'fill',
+			'source': 'states',
+			'layout': {},
+			'paint': {
+				'fill-color': '#E52C04',
+				'fill-opacity': [
+					'case',
+					['boolean', ['feature-state', 'hover'], false], 1, 0.5]
+			}
+		});
+		 
+		map.addLayer({
+			'id': 'state-borders',
+			'type': 'line',
+			'source': 'states',
+			'layout': {},
+			'paint': {
+				'line-color': '#E52C04',
+				'line-width': 2
+			}
+		});
+		
+
+		map.on('mousemove', 'state-fills', function (e) {
+			if (e.features.length > 0) {
+				if (hoveredStateId !== null) {
+					map.setFeatureState(
+						{ source: 'states', id: hoveredStateId },
+						{ hover: false }
+					);
+				}
+				hoveredStateId = e.features[0].id;
+				//console.log(e.features[0]);
+
+		    // Show the popup at the coordinates with some data
+		    popup.setLngLat(e.lngLat.wrap())
+		      .setHTML(checkEmpty("<p style='text-align: center;'>"+e.features[0].properties.admin_name+"<br> Infectados: "+infectados[e.features[0].properties.admin_name]+"</p>"))
+		      .addTo(map);
+
+				map.setFeatureState(
+					{ source: 'states', id: hoveredStateId },
+					{ hover: true },
+				);
+			}
+		});
+		 
+		map.on('mouseleave', 'state-fills', function () {
+			if (hoveredStateId !== null) {
+	    	popup.remove();
+				map.setFeatureState(
+					{ source: 'states', id: hoveredStateId },
+					{ hover: false }
+				);
+			}
+			hoveredStateId = null;
+		});
+	});
 }
